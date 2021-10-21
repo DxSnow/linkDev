b0bd2a98e40013b0577cdff27c8590e66d2efb5f
DxSnow <62160409+DxSnow@users.noreply.github.com>
2021-10-20 23:49:47 -0700

    create User model and its mango collection

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; //also need to require the Mongoose Schema
+
+//Create schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+module.exports = User = mongoose.model('users', UserSchema);
+// ^^ this means let mongoose take the UserSchema and create a real collection in MongoDB called 'users', then export it.
+//it is a combination of the following:
+//User = mongoose.model('users', UserSchema);
+//module.exports = User
