ed7280d3ae9876901f7abf7af67c03492211b9f4
DxSnow <62160409+DxSnow@users.noreply.github.com>
2021-10-20 23:13:07 -0700

    specified db import

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 //import is called require in JS
 const express = require('express');
 const app = express();
-const db = require('./config/keys');
+const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -18,13 +18,11 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 //Connect to MangoDB
-console.log(db.mongoURI);
-mongoose.connect(db.mongoURI)
+console.log(db);
+mongoose.connect(db)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
 const port = 5000;
 app.listen(port,()=>console.log(`Server is running on port ${port}`));
-//^listen is an Express method. Starts a UNIX socket and listens for connections on the given path. format: app.listen(path, [callback]) means if connection to path is successful, do the callback function.
-
-
+//^listen is an Express method. Starts a UNIX socket and listens for connections on the given path. format: app.listen(path, [callback]) 
