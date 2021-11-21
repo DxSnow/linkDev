c0db50d4dfdae135dd9feb4cbbd46280426e157b
DxSnow <62160409+DxSnow@users.noreply.github.com>
2021-11-20 19:51:00 -0800

    add Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
-import Footer from './components/layout/Footer';
 import './App.css';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+
 
 export default class App extends Component {
   render() { //render is the last step in a component's life cycle.
     return (
       <div className="App">
+        <Navbar/>
 
         <Footer/>
       </div>
