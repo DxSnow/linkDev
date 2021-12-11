bfd4ca2529576290b6692300c13319823747d202
DxSnow <62160409+DxSnow@users.noreply.github.com>
2021-12-10 18:07:16 -0800

    cleaned up some temperary code

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -40,11 +40,9 @@ class Register extends Component {
 
   render() {
     const {errors} = this.state;
-    const {user} = this.props.auth;
 
     return (
       <div className="register">
-        {user? user.name : "no user"}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,7 @@ import {createStore,applyMiddleware,compose} from 'redux';
 import rootReducer from './reducers'; //if no subfolder is specified, this folder's index.js is the entry point
 import thunk from 'redux-thunk';
 
-const middleware = [thunk]; //break data into smaller chunks
+const middleware = [thunk]; //break data into smaller chunks. middleware is array of thunk now.
 const store = createStore(
   rootReducer, //register reducers here
   {}, //set default value
